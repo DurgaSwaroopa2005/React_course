453278bc4241130929990dae984e15e1af05badf
DurgaSwaroopa2005 <akkireddydurgaswaroopa@gmail.com>
2026-06-09 20:36:31 +0530

    added css js

---
+++ App.js
@@ -0,0 +1,8 @@
+const heading = React.createElement("h1",
+    {id: "heading"},
+    "hello this is react!"
+);
+
+console.log(heading); //heading(React.createElement) here is a object
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
