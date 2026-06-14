9a1b1a72b2bd61233f4f14beb7aeba2c51bbb850
DurgaSwaroopa2005 <akkireddydurgaswaroopa@gmail.com>
2026-06-14 15:52:07 +0530

    nested tags2

--- App.js
+++ App.js
@@ -1,7 +1,18 @@
-const heading = React.createElement(
-    "div",
-    {},
-    React.createElement("h1",{},"nested one"));
+const heading = React.createElement("div",{id:"parent"},
+    [React.createElement("div",{id:"child1"},
+     [React.createElement("h1",{},"h1 tag"),
+      React.createElement("h2",{},"h2 tag")
+     ]
+    ),
+     React.createElement("div",{id:"child2"},
+     [React.createElement("h1",{},"h1 tag"),
+      React.createElement("h2",{},"h2 tag")
+     ]
+    )
+    ]
+)
+
+
 
 console.log(heading); //heading(React.createElement) here is a object
 
@@ -10,3 +21,15 @@ root.render(heading);
 
 
 
+// <div id="root">
+//     <div id="parent">
+//         <div id="child1">
+//             <h1></h1>
+//             <h2></h2>
+//         </div>
+//         <div id="child2">
+//             <h1>h1</h1>
+//             <h2>h2</h2>
+//         </div>
+//     </div>
+// </div>
